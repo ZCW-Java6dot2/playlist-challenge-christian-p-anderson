5bb9b91b3b463466271c51c1da1ec95931713999
christian <christian@zipcodes-MBP-51.lan>
2020-11-24 17:47:25 -0500

    solved as math problem

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -12,42 +12,10 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(this.playList));
+        ArrayList<String> songList = new ArrayList<String>(Arrays.asList(this.playList));
 
-        String startSong = arrayList.get(startIndex);
-
-        Integer fwdDistance = Integer.MAX_VALUE;
-        Integer revDistance = Integer.MIN_VALUE;
-
-//        move forwards
-        for (int i = 0; i < arrayList.size(); i++) {
-            for (int j = i + 1; j < arrayList.size(); j++) {
-                if ((startSong.equals(playList[i]) && selection.equals(playList[j]) || selection.equals(playList[i]) && startSong.equals(playList[j]))
-                        && fwdDistance > Math.abs(i - j)) {
-                    fwdDistance = Math.abs(i - j);
-                }
-
-            }
-        }
-
-//        move backwards
-
-        for (int i = arrayList.size() - 1; i >= 0; i--) {
-            for (int j = i - 1; j >= 0; j--) {
-                if ((startSong.equals(playList[i]) && selection.equals(playList[j]) || selection.equals(playList[i]) && startSong.equals(playList[j]))
-                        && revDistance < Math.abs(i - j)) {
-                    revDistance = Math.abs(i - j);
-                }
-
-            }
-        }
-
-        if (fwdDistance < revDistance) {
-            return fwdDistance;
-        } else if (fwdDistance.equals(revDistance)){
-            return fwdDistance;
-        } else {
-            return revDistance;
-        }
+        Integer forwardCount = Math.abs(songList.indexOf(selection) - startIndex);
+        Integer reverseCount = Math.abs((songList.size() + startIndex) - songList.lastIndexOf(selection));
+        return Math.min(forwardCount, reverseCount);
     }
 }

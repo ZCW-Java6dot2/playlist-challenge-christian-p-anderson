05d34e588e3382bc5f1d671a1c0cb538afdfc126
christian <christian@zipcodes-MBP-51.lan>
2020-11-24 17:39:03 -0500

    got to evaluate looking forward

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +12,42 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(this.playList));
+
+        String startSong = arrayList.get(startIndex);
+
+        Integer fwdDistance = Integer.MAX_VALUE;
+        Integer revDistance = Integer.MIN_VALUE;
+
+//        move forwards
+        for (int i = 0; i < arrayList.size(); i++) {
+            for (int j = i + 1; j < arrayList.size(); j++) {
+                if ((startSong.equals(playList[i]) && selection.equals(playList[j]) || selection.equals(playList[i]) && startSong.equals(playList[j]))
+                        && fwdDistance > Math.abs(i - j)) {
+                    fwdDistance = Math.abs(i - j);
+                }
+
+            }
+        }
+
+//        move backwards
+
+        for (int i = arrayList.size() - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if ((startSong.equals(playList[i]) && selection.equals(playList[j]) || selection.equals(playList[i]) && startSong.equals(playList[j]))
+                        && revDistance < Math.abs(i - j)) {
+                    revDistance = Math.abs(i - j);
+                }
+
+            }
+        }
+
+        if (fwdDistance < revDistance) {
+            return fwdDistance;
+        } else if (fwdDistance.equals(revDistance)){
+            return fwdDistance;
+        } else {
+            return revDistance;
+        }
     }
 }
